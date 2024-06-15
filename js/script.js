ca3046d2f277b24c6f660dcffa93c69bb5369b91
cabrinilm <luca_cabrinimm@hotmail.com>
2024-06-15 13:51:34 +0100

    inicio js feliz concluido

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+const dado = document.getElementById('dado');
+
+const adviceIdElement = document.querySelector('.advice-id');
+
+const adviceTextElement = document.querySelector('.advice-text')
 
 
+const happyQuotes = [
+    "A felicidade não é algo pronto. Ela vem de suas próprias ações.",
+    "A verdadeira felicidade custa pouco; se é cara, não é de boa qualidade.",
+    "Ser feliz sem motivo é a mais autêntica forma de felicidade.",
+    "A felicidade não se resume na ausência de problemas, mas sim na sua capacidade de lidar com eles.",
+    "A felicidade é a única coisa que podemos dar sem possuir.",
+];
  
+
+
+
+
+
+dado.addEventListener('click', function() {
+    // Altera o texto do <h1>
+    adviceIdElement.textContent = 'Estamos felizes por saber como você está se sentindo! Aqui está uma frase que celebra sua felicidade.';
+
+    setTimeout(function() {
+        // Seleciona aleatoriamente uma frase do array
+        const randomQuote = happyQuotes[Math.floor(Math.random() * happyQuotes.length)];
+        // Altera o texto do elemento <p> com a classe 'advice-text'
+        adviceTextElement.textContent = randomQuote;
+    }, 2000); // 2000 milissegundos (2 segundos)
+});
+
